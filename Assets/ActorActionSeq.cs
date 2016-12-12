bf2784edadf75f968131fe1cdca9eef636113176
igal nassima <inassima@gmail.com>
2016-12-11 21:23:05 -0500

    first love

---
+++ Assets/ActorActionSeq.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CinemaDirector
+{
+	[System.Serializable]
+	public class ColliderAction {
+
+		public GameObject collider;
+		public Cutscene cutscene;
+
+	}
+	/// <summary>
+	/// Plays through a list of given cutscenes one by one.
+	/// </summary>
+	public class ActorActionSeq : MonoBehaviour
+	{
+		public List<ColliderAction> ColliderActions;
+		private int index = 0;
+		bool loop = false;
+
+		/// <summary>
+		/// Play the first cutscene and waits for it to finish
+		/// </summary>
+		void Start()
+		{
+			int i = 0;
+			foreach (ColliderAction cutscene in ColliderActions) {
+				CollisionHandler colhandlr = cutscene.collider.AddComponent<CollisionHandler> ();
+				colhandlr.index = i;
+				colhandlr.onCollisionTriggered += ObjectCollisionTriggered;
+				i++;
+			}
+		}
+
+		void Test() {
+
+			if (ColliderActions.Count > 0) {
+				ColliderActions [index].cutscene.CutsceneFinished += CutsceneQueue_CutsceneFinished;
+				ColliderActions [index].cutscene.Play ();
+
+			}
+		}
+
+		void ObjectCollisionTriggered(object sender, CollisionEventArgs e) {
+			Debug.Log ("ok collider " + e.index);
+
+			for (int k = 0; k < ColliderActions.Count; k++) {
+				if (k == e.index) {
+					ColliderActions [e.index].cutscene.CutsceneFinished += CutsceneQueue_CutsceneFinished;
+					ColliderActions [e.index].cutscene.Play ();
+				} else {
+					ColliderActions [k].collider.SetActive (false);
+				}
+			}
+		}
+
+		/// <summary>
+		/// On cutscene finish, play the next cutscene.
+		/// </summary>
+		void CutsceneQueue_CutsceneFinished(object sender, CutsceneEventArgs e)
+		{
+			Debug.Log (e.targetScene);
+			e.targetScene.CutsceneFinished -= CutsceneQueue_CutsceneFinished;
+			for (int k = 0; k < ColliderActions.Count; k++) {
+				ColliderActions [k].collider.SetActive (true);
+			}
+
+			if (loop) {
+				index++;
+				if (index == ColliderActions.Count) {
+					index = 0;
+				} 
+
+				ColliderActions [index].cutscene.CutsceneFinished += CutsceneQueue_CutsceneFinished;
+				ColliderActions [index].cutscene.Play ();
+			}
+		}
+
+		/// <summary>
+		/// Disables all collider objects.
+		/// </summary>
+		void DisableColliderObjects() {
+
+		}
+	}
+}
---
+++ Assets/CollisionHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CollisionHandler : MonoBehaviour {
+
+	// Event fired when Cutscene starts to play or is resumed.
+	public event CollisionTriggeredHandler onCollisionTriggered;
+	public int index;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("other " + other.name);
+		if (onCollisionTriggered != null)
+		{
+			onCollisionTriggered(this, new CollisionEventArgs(index));
+		}
+	}
+
+
+}
+
+// Delegate for handling Cutscene Events
+public delegate void CollisionTriggeredHandler(object sender, CollisionEventArgs e);
+
+/// <summary>
+/// Cutscene event arguments. Blank for now.
+/// </summary>
+public class CollisionEventArgs : EventArgs
+{
+
+	public int index;
+
+	public CollisionEventArgs()
+	{
+
+	}
+
+	public CollisionEventArgs(int _index)
+	{
+		index = _index;
+	}
+}
